1490f4d4d3c4343ed2a760f5ba58cbcb66169044
wardou2 <ward92@gmail.com>
2019-10-10 22:31:20 -0700

    clean up code

--- public/src/album.js
+++ public/src/album.js
@@ -1,5 +1,4 @@
-const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
 ];
 let ALBUMS = []
 
